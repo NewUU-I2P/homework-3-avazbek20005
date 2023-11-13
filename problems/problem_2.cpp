599b014e69e48c30e70f4aaecdb7a750cccec329
avazbek20005 <147366696+avazbek20005@users.noreply.github.com>
2023-11-13 20:24:52 +0500

    Update problem_2.cpp

--- problems/problem_2.cpp
+++ problems/problem_2.cpp
@@ -1,6 +1,9 @@
 float problemSolution2(float i, float j, float k) {
     float result;
     // write your code here;
+       
+    double result = (i> j) ? ((i > k) ? i : k) : ((j > k) ? j : k)
+    
 
     return result;
 }
